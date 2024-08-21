a20f12d662ba2e7d8a98042d2bcf8341866e2da1
samirvithlani <samir.vithlani83955@gmail.com>
2024-08-21 16:38:44 +0530

    post api

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import { FormDemo4 } from "./forms/FormDemo4";
 import { FormDemo5 } from "./forms/FormDemo5";
 import { FormDemo6 } from "./forms/FormDemo6";
 import { ApiDemo1 } from "./api/ApiDemo1";
+import { ApiDemo2 } from "./api/ApiDemo2";
 
 function App() {
  
@@ -39,6 +40,7 @@ function App() {
         <Route path="/formdemo4" element = {<FormDemo4/>}></Route>
         <Route path="/formdemo5" element = {<FormDemo6/>}></Route>
         <Route path ="/apidemo1" element = {<ApiDemo1/>}></Route>
+        <Route path ="/apidemo2" element = {<ApiDemo2/>}></Route>
         {/* <Route path ="/*" element = {<h1>404</h1>}></Route> */}
         <Route path="/*" element ={<Error404/>}></Route>
       </Routes>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -56,6 +56,11 @@ export const Navbar = () => {
                 apidemo1
               </Link>
             </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="/apidemo2">
+                apidemo2
+              </Link>
+            </li>
           </ul>
         </div>
       </nav>
